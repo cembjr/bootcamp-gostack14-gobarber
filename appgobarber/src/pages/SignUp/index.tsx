4ed2795dd5d2a4df6ff5f514b45e22cb1ede52ce
Carlos Eduardo M Barreto Jr <carlos_edujr@hotmail.com>
2020-11-16 21:00:26 -0300

    Estrutura de Padrões iniciais do projeto. Inicio da estilizaçãão a págiina de login. Implementação de Rotas

---
+++ appgobarber/src/pages/SignUp/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Container } from './styles';
+
+export const SignUp: React.FC = () => {
+  return (
+    <>
+      <Container />
+    </>
+  );
+};
---
+++ appgobarber/src/routes/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { SignUp } from '../pages/SignUp';
+import { SignIn } from '../pages/SignIn';
+
+const Auth = createStackNavigator();
+
+export const Route: React.FC = () => {
+  return (
+    <>
+      <Auth.Navigator
+        screenOptions={{
+          headerShown: false,
+          cardStyle: { backgroundColor: '#312e38' },
+        }}
+      >
+        <Auth.Screen name="SignIn" component={SignIn} />
+        <Auth.Screen name="SignUp" component={SignUp} />
+      </Auth.Navigator>
+    </>
+  );
+};
